a44c770185ea5ea2a02bb8339bd13466313fe0ce
AllenCodess <Aph296@protonmail.com>
2025-09-17 19:34:39 -0400

    working on lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.querySelector("#create-task-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const input = document.querySelector("#new-task-description");
+  const taskName = input.value;
+
+  buildToDo(taskName);
+
+  input.value = "";
 });
+
+function buildToDo(taskName) {
+  const li = document.createElement("li");
+  li.textContent = taskName;
+  const taskList = document.querySelector("#tasks");
+  taskList.appendChild(li);
+}
